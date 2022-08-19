a774476877ba1a401e0dbe1ed6cb734c80066406
@dank1910 <53305399+ewigspace1910@users.noreply.github.com>
2022-08-20 00:22:14 +0700

    code4shortpaper

---
+++ script/train.sh
@@ -0,0 +1,5 @@
+python examples/train.py --c configs/cosloss.yaml --n 4 > logs/cosloss.log 
+python examples/train.py --c configs/arcloss.yaml --n 4 > logs/arcloss.log 
+python examples/train.py --c configs/magloss.yaml --n 4 > logs/magloss.log 
+python examples/train.py --c configs/softmax.yaml --n 4 > logs/softmax.log 
+
